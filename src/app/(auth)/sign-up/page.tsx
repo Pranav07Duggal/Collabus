157477f91260a98095bc9b471e6b5cc3bb51fd8e
Batman <batman@Batman.Batman>
2025-06-12 12:49:36 +0530

    auth setup

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+
+import { SignUpView } from '@/modules/auth/ui/views/sign-up-view'
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <SignUpView/>
+  )
+}
+
+export default SignIn
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,47 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 
 export default function Home() {
+  const {data: session} = authClient.useSession() 
+
+  const [name,setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      name,
+      password,
+    }, {
+      onError:() => {
+        window.alert("Something went wrong");
+      },
+      onSuccess:()=>{
+        window.alert("Success");
+      }
+    });
+  }
+  if(session){
+    return(
+      <div className="flex flex-col p-4 gap-y-4">
+        <h1>
+        Logged In as {session.user.name}
+      </h1>
+      <Button onClick={()=>{authClient.signOut()}}>Sign Out</Button>
+      </div>
+    )
+  }
   return (
-    <>Hello World</>  );
+    <div className="p-4 flex flex-col gap-y-4 ">
+      <Input placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+      <Input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <Input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+      <Button onClick={onSubmit}>Create User</Button>
+    </div>   
+    );
 }
